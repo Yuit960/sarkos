96f8466ef6e47e5c54e311f4ad1b93c5577ce4d2
Aurélien Pouillès <aurelien.pouilles.pro@hotmail.com>
2025-10-01 10:07:03 +0200

    Q2_tp0

--- tp0/tp.c
+++ tp0/tp.c
@@ -9,4 +9,7 @@ extern uint32_t __kernel_end__;
 void tp() {
    debug("kernel mem [0x%p - 0x%p]\n", &__kernel_start__, &__kernel_end__);
    debug("MBI flags 0x%x\n", info->mbi->flags);
+   debug("mmap addr 0x%x len 0x%x\n",
+         info->mbi->mmap_addr, info->mbi->mmap_length);
+
 }
